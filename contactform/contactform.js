a8eec2ce68c82f5b88b34c7a0b1db2df74f05a0a
kpashir <87796374+kpashir@users.noreply.github.com>
2021-07-22 16:17:14 +0530

    Update contactform.js

--- contactform/contactform.js
+++ contactform/contactform.js
@@ -92,7 +92,7 @@ jQuery(document).ready(function($) {
     else var str = $(this).serialize();
     $.ajax({
       type: "POST",
-      url: "",
+      url: "https://script.google.com/macros/s/AKfycbwyQUUQ3Ksolix5yx3YfdNFUZr-gAle5UiW_d_fBJKLkehzC0PxytK8FdU0Z_HMVHVL/exec",
       data: str,
       success: function(msg) {
         // alert(msg);
